c1c286c307f424ecc4673c786337ade70e0da263
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-26 04:02:23 -0400

    done

--- src/controllers/MyReastaurantController.ts
+++ src/controllers/MyReastaurantController.ts
@@ -3,6 +3,25 @@ import cloudinary from "cloudinary";
 
 import Restaurant from "../models/reastaurant";
 import mongoose from "mongoose";
+import Order from "../models/order";
+
+const getMyRestaurantOrder = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+
+    const orders = await Order.find({ restaurant: restaurant._id })
+      .populate("restaurant")
+      .populate("user");
+
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 const getMyRestaurant = async (req: Request, res: Response) => {
   try {
@@ -44,7 +63,6 @@ const createMyRestaurant = async (req: Request, res: Response) => {
 };
 
 const updateMyRestaurant = async (req: Request, res: Response) => {
-  
   try {
     const reastaurant = await Restaurant.findOne({ user: req.userId });
     if (!reastaurant) {
@@ -83,8 +101,41 @@ const uploadImage = async (file: Express.Multer.File) => {
   return uploadResponse.url;
 };
 
+const updateOrderStatus = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+  
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({ message: "order not found" });
+    }
+
+    const restaurant = await Restaurant.findById(order.restaurant);
+    
+    if (restaurant?.user?._id.toString() !== req.userId) {
+      return res.status(401).send();
+    }
+    console.log(restaurant)
+
+    order.status = status;
+    console.log(order)
+    await order.save();
+
+    res.status(200).json(order);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "unable to update order status" });
+  }
+};
+
+
 export default {
   createMyRestaurant,
   getMyRestaurant,
   updateMyRestaurant,
+  getMyRestaurantOrder,
+  updateOrderStatus
 };
